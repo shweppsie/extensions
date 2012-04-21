04ca7e30cf749dd3f71c43b6cb1fa05987aed2b3
Nathan Overall <git@shweppsie.com>
2012-04-22 01:49:58 +1200

    Fix broken urls
    * fix timing out by only loading trackers on hover
    * indicate links with trackers by changing them to red from grey
    * fix undefined trackers variable
    * detect null variables

--- torrentz/replace.js
+++ torrentz/replace.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 	// Magnet Image: http://magnet-uri.sourceforge.net/
-	var ImageData = 'data:image/gif;base64,R0lGODlhDgAOAPcAAAAAAGNjY97e3v8AAP8ICP////////////////////////' +
+	var RedImageData = 'data:image/gif;base64,R0lGODlhDgAOAPcAAAAAAGNjY97e3v8AAP8ICP////////////////////////' +
 					'//////////////////////////////////////////////////////////////////////////////////////' +
 					'//////////////////////////////////////////////////////////////////////////////////////' +
 					'//////////////////////////////////////////////////////////////////////////////////////' +
@@ -16,6 +16,8 @@ $(document).ready(function(){
 					'/////////////////////////////////yH5BAEAAAUALAAAAAAOAA4AAAhhAAEIHFigwMCDAhIKAFAQgMKFDh' +
 					'UyNPhQYMWGFQEMGEAAQICCAQAQGClwY8eCDUd21MhxYkqSLE+iFAlz4wCPIGmutHlzoMqOAXja/EkgQEihHFXi' +
 					'DMnS5MqPKI0eNIoyIAA7';
+
+	var GreyImageData = 'data:image/gif;base64,R0lGODlhDgAOAMIFAAAAAGNjY97e3v8AAP8ICP///////////yH5BAEAAAcALAAAAAAOAA4AAAM1CLp3+yIK0ICcVlJ3Va8dEIxiE4gkejbVeKpbm7qxQ8OBCauv4r6/IO0k/JWAQR/r0FvkGgkAOw==';
 
 	// URL Regex
 	var urlExpr = /\/([a-f0-9]{40})/;
@@ -45,19 +47,19 @@ $(document).ready(function(){
 		var magnetUri = "magnet:?xt=urn:btih:" + infoHash + "&dn=" + title + trackers;
 
 		// Magnet Link
-		var newHTML = '<dl><dt><a href="' + magnetUri + '"><span class="u" style="background: transparent url(' + ImageData +') no-repeat 5px center;" font-size: 16px">Magnet Link</span></a></dt></dl>';
-		
+		var newHTML = '<dl><dt><a href="' + magnetUri + '"><span class="u" style="background: transparent url(' + RedImageData +') no-repeat 5px center;" font-size: 16px">Magnet Link</span></a></dt></dl>';
+
 		var links = $("dl", downloadBlock)
 		for(i in links){
 			links.eq(i).hide();
 		}
-		
+
 		var otherDownloads = '<dl class="otherDownloads"><dt><a><span class="u" font-size: 16px">Other Downloads</span></a></dt></dl>';
 
 		// Add New Links
 		links.eq(0).before(newHTML);
 		links.eq(0).before(otherDownloads);
-		
+
 		$('dl.otherDownloads').bind(
 			"click",
 			function(){
@@ -83,40 +85,68 @@ $(document).ready(function(){
 			// Grab the torrent details
 			var link = $("a", dt);
 			var title = link.text();
-			var infoHash = link.attr('href').match(urlExpr)[1];
+
+			var href = link.attr('href');
+			if(href == null)
+				return;
+
+			var infoHash = href.match(urlExpr);
+			if(infoHash == null)
+				return;
+
+			// Get the hash from the link
+			infoHash = infoHash[1]
 
 			// Set the initial magnet link without trackers, we'll update it later
-			dt.innerHTML = '<a href="magnet:?xt=urn:btih:' + infoHash + '&dn=' + title + '" title="Magnet Link"><img src="' + ImageData + '" alt="Magnet Link" border="0" width="14" height="14"/></a>&nbsp;' + dt.innerHTML;
+			dt.innerHTML = '<a href="magnet:?xt=urn:btih:' + infoHash + '&dn=' + title + '" title="Magnet Link"><img src="' + GreyImageData + '" alt="Magnet Link" border="0" width="14" height="14"/></a>&nbsp;' + dt.innerHTML;
 
 			// Grab the link so we can update the URI later
-			link = $("a", dt).get(0);
+			link = $("a", dt).first();
 
-			// Ajax Update function
-			var update = (function(pLink, pTitle, pHash){
-				return function(response) {
+			// ajax function to add trackers to each link
+			link.hover( function () {
+				// Don't do this if we've done it before
+				if(!link.hasClass('tracked')){
+					// Asynchronous grab for trackers
+					$.ajax({
+						url: window.location.protocol + "//" + window.location.hostname + "/" + infoHash,
+						dataType: "text",
+						tryCount : 0,
+						retryLimit : 3,
+						success : function(response) {
+							// Regex to catch trackers
+							var regex = /tracker_[0-9]*\">([^<]*)</g
 
-					// Regex to catch trackers
-					var regex = /tracker_[0-9]*\">([^<]*)</g
+							// Generate the tracker list
+							var trackers = "";
+							var match = regex.exec(response);
+							while (match != null){
+								trackers += "&tr=" + escape(match[1]);
+								match = regex.exec(response);
+							}
 
-					// Generate the tracker list
-					match = regex.exec(response);
-					while (match != null){
-						trackers += "&tr=" + match[1];
-						match = regex.exec(response);
-					}
-
-					// Generate the new url
-					pLink.href = "magnet:?xt=urn:btih:" + pHash + "&dn=" + pTitle + trackers;
-				};
-			})(link, title, infoHash);
-
-			// Asynchronous grab for trackers
-			$.ajax({
-				url: window.location.protocol + "//" + window.location.hostname + "/" + infoHash,
-				dataType: "text",
-				success: update
+							// Generate the new url
+							var href = "magnet:?xt=urn:btih:" + infoHash + "&dn=" + title + trackers;
+							link.attr("href", href);
+							$("img",link).first().attr("src", RedImageData);
+							link.addClass('tracked')
+						},
+						error : function(xhr, textStatus, errorThrown ) {
+							if (errorThrown == 'Service Temporarily Unavailable') {
+								this.tryCount++;
+								if (this.tryCount <= this.retryLimit) {
+									//try again
+									console.log("retrying...");
+									$.ajax(this);
+									return;
+								}
+								console.log("Failed to add trackers to magnet link!");
+								return;
+							}
+						}
+					});
+				}
 			});
-
 		});
 	});
 });
